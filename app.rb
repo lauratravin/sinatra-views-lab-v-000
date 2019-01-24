8da282242fc0370944af356a8b2cfeb3bbdbd2b2
Laura Messa Travin <laura.travin@gmail.com>
2019-01-24 21:51:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,12 +3,15 @@ class App < Sinatra::Base
 	get '/' do
 		erb :index
 	end
+
 	get '/hello' do
 		erb :hello
 	end
+
 	get '/date' do
 		erb :date
 	end
+	
 	get '/goodbye' do
 		erb :goodbye
 	end
